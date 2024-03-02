0a74866aadfb59d211ebfb44ce687149a29a1f11
James Victor Alvarez <89657389+JamesVictorAlvarez@users.noreply.github.com>
2024-03-01 22:06:21 -0500

    Added parallax tilt library & some css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './home.css'; 
 
 function Home() {
   return (
-    <div className="container">
-        <h1>Welcome to My Blog</h1>
-        <p>TEST TEST</p>
+    <div> {/* No extra container needed if using parallax library */}
+      <section className="stars-section">
+        <div className="description">
+          <h1>Welcome to My Space-Themed Blog</h1>
+          <p>A little about my adventures in tech, coding, and the vast unknown...</p>
+        </div>
+      </section>
     </div>
   );
 }
