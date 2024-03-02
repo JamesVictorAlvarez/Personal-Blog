1f52733bc3a28f03960ae873b9a8b88023e27eb7
James Victor Alvarez <89657389+JamesVictorAlvarez@users.noreply.github.com>
2024-03-01 20:51:53 -0500

    Init components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import './App.css'; 
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Videos from './components/Videos';
+import Github from './components/Github';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/videos" element={<Videos />} />
+          <Route path="/github" element={<Github />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
